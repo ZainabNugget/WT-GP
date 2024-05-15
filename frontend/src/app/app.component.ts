1487074fb138b387af5331d4696b37ab87a214df
Rezii-xi <ericgeraghty30@gmail.com>
2024-05-15 16:57:25 +0100

    Added CSS to homepage
    -added blog cards
    -added sidebar
    -added feature post

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-=======
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
->>>>>>> origin/zainab
 
 @Component({
   selector: 'app-root',
@@ -16,13 +12,10 @@ import { NavigationComponent } from './components/navigation/navigation.componen
   imports: [
     RouterOutlet,
     NavigationComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
+    FooterComponent,
     LoginComponent,
     AdminComponent,
     HomeComponent
->>>>>>> origin/zainab
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
